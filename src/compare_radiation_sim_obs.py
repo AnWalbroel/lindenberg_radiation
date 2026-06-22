ba14fa2963e9aef12cd6609d8c6b6f230138302a
AnWalbroel <walbroel.andreas@gmail.com>
2026-06-22 19:11:33 +0200

    feat: add script to compare simulated and observed radiation data

---
+++ src/compare_radiation_sim_obs.py
@@ -0,0 +1,53 @@
+import os
+import pdb
+
+import numpy as np
+import xarray as xr
+import matplotlib as mpl
+mpl.use("WebAgg")
+import matplotlib.pyplot as plt
+
+import _paths
+from readers.read_radiation_sim import read_pyRRTMG_simulation
+from readers.read_radiation_obs import read_bsrn
+
+def main():
+    
+    path_plots = os.path.join(os.environ['PATH_PLOTS_BASE'],
+                              "compare_radiation_sim_obs/")
+    
+    date_str = "2025-10-01"
+    date = np.datetime64(date_str)
+    ds_obs = read_bsrn(date0=date_str)
+    ds_sim = read_pyRRTMG_simulation(date=date_str)
+    
+    ds_sim = ds_sim.isel(height_h=0)
+    ds_sim['swdifflx'] = compute_diffuse_from_direct_and_global_radiation(ds_sim['swdirflx'], ds_sim['swdflx'])
+    
+    f1, axs = plt.subplots(4,1, sharex=True, figsize=(10,10))
+    
+    vars_obs = ["rsds", "rsdifds", "rsdirds", "rlds"]
+    vars_sim = ["swdflx", "swdifflx", "swdirflx", "lwdflx"]
+    for ax, var_obs, var_sim in zip(axs, vars_obs, vars_sim):
+        ax.plot(ds_obs.time, ds_obs[var_obs], label='obs')
+        ax.plot(ds_sim.time, ds_sim[var_sim], label='sim')
+        
+        lh, ll = ax.get_legend_handles_labels()
+        ax.legend(lh, ll, loc='upper right', frameon=False)
+        ax.set_ylabel(" ".join([var_sim, "(W$\,$m$^{-2}$)"]))
+    
+    plt.show()
+    pdb.set_trace()
+    
+    # TODO: respect obs flags
+    # TODO: check out _max and _min values
+    # TODO: check heating rates at top of simulated profile
+
+
+def compute_diffuse_from_direct_and_global_radiation(sw_direct: xr.DataArray, sw_global: xr.DataArray):
+    
+    return sw_global - sw_direct
+
+
+if __name__ == "__main__":
+    main()
